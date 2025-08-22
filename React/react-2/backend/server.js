dd5dc4a67c6cb019e53da053b4bf5ebf6b0845f5
Felipe Coltrinari <coltrinarifelipe@gmail.com>
2025-08-22 19:28:44 -0300

    cmbios en el back

--- React/react-2/backend/server.js
+++ React/react-2/backend/server.js
@@ -1,25 +1,22 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
 import { connectDB } from "./connect.js";
-import path from 'path';
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 
-app.use(express.static(path.join(process.cwd(), '../frontend/dist')));
-
-app.get('/api/usuarios', async (req, res) => {
-    const DB = await connectDB();
-    if (!DB) return res.status(500).json({ error: 'No se pudo conectar a la base de datos' });
-    const [rows] = await DB.execute("SELECT * FROM usr");
-    res.json(rows);
+app.get("/api/usuarios", async (req, res) => {
+  const DB = await connectDB();
+  if (!DB)
+    return res
+      .status(500)
+      .json({ error: "No se pudo conectar a la base de datos" });
+  const [rows] = await DB.execute("SELECT * FROM usr");
+  res.json(rows);
 });
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(process.cwd(), '../frontend/dist', 'index.html'));
-});
-
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-})
+  console.log(`Server running on http://localhost:${PORT}`);
+});
